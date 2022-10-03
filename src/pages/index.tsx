9d0e4aca82269305ce6b104423eb7700e1143009
Davi Lima <davi.rlima04@gmail.com>
2022-10-03 16:54:22 -0400

    estrutura da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,42 @@
+import Image from "next/future/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
+import camiseta3 from "../assets/camisetas/3.png";
+import camiseta4 from "../assets/camisetas/4.png";
+
 export default function Home() {
-  return <h1>Teste</h1>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta W</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta4} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta Z</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
