66e4dc779ed2807a71e3b1d471d3bb9a60079010
Davi Lima <davi.rlima04@gmail.com>
2022-10-05 17:42:35 -0400

    estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,35 @@
+import Image from "next/future/image";
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
+
+import camiseta from "../../assets/camisetas/1.png";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={camiseta} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+          animi officiis amet qui veniam impedit recusandae culpa error esse
+          consequatur est repellat tempora porro voluptates reiciendis officia
+          in, fugiat voluptatum.
+        </p>
+
+        <button>Compre agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
