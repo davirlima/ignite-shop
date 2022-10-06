e4309c47d573724c80f1a9c511a8b452434dbd37
Davi Lima <davi.rlima04@gmail.com>
2022-10-06 11:42:07 -0400

    estrutura da pagina de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,24 @@
+import Image from "next/future/image";
+import Link from "next/link";
+import { SuccessContainer, ImageContainer } from "../styles/pages/success";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+        <Image src="" alt="" width={130} height={145} />
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Davi Lima</strong>, sua <strong>Camiseta X</strong> já
+        está a caminho da sua casa.
+      </p>
+
+      <Link href={"/"}>
+        <a>Voltar ao catálogo</a>
+      </Link>
+    </SuccessContainer>
+  );
 }
