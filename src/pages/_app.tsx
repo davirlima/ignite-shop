a769cf680ee379d79718adb53be74fe34cb0ec6c
Davi Lima <davi.rlima04@gmail.com>
2022-10-09 23:42:24 -0400

    adicionando botão para o carrinho no header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from "../styles/global";
 import logoImg from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
 import Image from "next/future/image";
+import { Handbag } from "phosphor-react";
 
 globalStyles();
 
@@ -11,6 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
+        <button>
+          <Handbag size={24} color="#8d8d99" weight="bold" />
+        </button>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -10,8 +10,21 @@ export const Container = styled("div", {
 });
 
 export const Header = styled("header", {
+  display: "flex",
+  justifyContent: "space-between",
   padding: "2rem 1rem",
   width: "100%",
   maxWidth: 1180,
   margin: "0 auto",
+
+  button: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 48,
+    height: 48,
+    borderRadius: 6,
+    backgroundColor: "#202024",
+    border: "none",
+  },
 });
