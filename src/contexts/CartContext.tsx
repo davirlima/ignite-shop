b57bb0953a8dbd91f16a33e7faab6ead1a3e748e
Davi Lima <davi.rlima04@gmail.com>
2022-10-10 23:33:00 -0400

    adicionando funções no contexto

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,17 +1,20 @@
 import { createContext, ReactNode, useState } from "react";
+import { produce } from "immer";
 
 interface Product {
   id: string;
   name: string;
   imageUrl: string;
   price: string;
-  description: string;
-  defaultPriceId: string;
+  description?: string;
+  defaultPriceId?: string;
 }
 
 interface CartContextProps {
   productsOnCart: Product[];
+  totalItemsOnCart: number;
   addProductOnCart: (product: Product) => void;
+  removeProductFromCart: (product: Product) => void;
 }
 
 interface CartContextProviderProps {
@@ -23,18 +26,40 @@ export const CartContext = createContext({} as CartContextProps);
 export function CartContextProvider({ children }: CartContextProviderProps) {
   const [productsOnCart, setProductOnCart] = useState<Product[]>([]);
 
-  function addProductOnCart(product: Product) {
-    const verificator = productsOnCart.findIndex(
-      (product) => product.id === product.id
-    );
+  const totalItemsOnCart = productsOnCart.length;
 
-    if (verificator != -1) {
+  function verifyIfProductAlreadyStayOnCart(productId: string) {
+    const verificator = productsOnCart.findIndex(
+      (product) => product.id === productId
+    );
+    return verificator != -1 ? true : false;
+  }
+
+  function addProductOnCart(product: Product) {
+    if (!verifyIfProductAlreadyStayOnCart(product.id)) {
       setProductOnCart((state) => [...state, product]);
     }
   }
 
+  function removeProductFromCart(product: Product) {
+    const position = productsOnCart.findIndex(
+      (oldProduct) => oldProduct.id === product.id
+    );
+    const newCart = produce(productsOnCart, (draft) => {
+      draft.splice(position, 1);
+    });
+    setProductOnCart(newCart);
+  }
+
   return (
-    <CartContext.Provider value={{ productsOnCart, addProductOnCart }}>
+    <CartContext.Provider
+      value={{
+        productsOnCart,
+        addProductOnCart,
+        totalItemsOnCart,
+        removeProductFromCart,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
