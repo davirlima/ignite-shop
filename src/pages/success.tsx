7ac15bebc9942b9860f167d124269f12c50a7336
Davi Lima <davi.rlima04@gmail.com>
2022-10-11 10:50:29 -0400

    remodelação da página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -2,19 +2,21 @@ import { GetServerSideProps } from "next";
 import Image from "next/future/image";
 import Head from "next/head";
 import Link from "next/link";
+
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
+
 import { SuccessContainer, ImageContainer } from "../styles/pages/success";
 
 interface SuccessProps {
   customerName: string;
-  product: {
+  products: {
     name: string;
     imageUrl: string;
-  };
+  }[];
 }
 
-export default function Success({ customerName, product }: SuccessProps) {
+export default function Success({ customerName, products }: SuccessProps) {
   return (
     <>
       <Head>
@@ -24,15 +26,36 @@ export default function Success({ customerName, product }: SuccessProps) {
       </Head>
 
       <SuccessContainer>
+        <div>
+          {products.map((product) => {
+            return (
+              <ImageContainer key={product.name}>
+                <Image
+                  src={product.imageUrl}
+                  alt=""
+                  width={130.18}
+                  height={142.69}
+                />
+              </ImageContainer>
+            );
+          })}
+        </div>
+
         <h1>Compra efetuada!</h1>
 
-        <ImageContainer>
-          <Image src={product.imageUrl} alt="" width={130} height={145} />
-        </ImageContainer>
-
         <p>
-          Uhuul <strong>{customerName}</strong>, sua{" "}
-          <strong>{product.name}</strong> já está a caminho da sua casa.
+          Uhuul <strong>{customerName}</strong>,{" "}
+          {products.length == 1 ? (
+            <>
+              sua <strong>{products[0].name}</strong> já está a caminho da sua
+              casa
+            </>
+          ) : (
+            <>
+              suas <strong>{products.length} camisetas</strong> já estão a
+              caminho da sua casa.
+            </>
+          )}
         </p>
 
         <Link href={"/"}>
@@ -59,16 +82,22 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     expand: ["line_items", "line_items.data.price.product"],
   });
 
+  console.log(session);
+
   const customerName = session.customer_details.name;
-  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  const products = session.line_items.data.map((item) => {
+    const product = item.price.product as Stripe.Product;
+    return {
+      name: product.name,
+      imageUrl: product.images[0],
+    };
+  });
 
   return {
     props: {
       customerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0],
-      },
+      products: products,
     },
   };
 };
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -11,21 +11,31 @@ export const SuccessContainer = styled("main", {
   maxWidth: 1180,
   padding: "0 1rem 1rem 1rem",
 
+  "& > div": {
+    display: "flex",
+
+    "& > div:nth-child(n + 2)": {
+      marginLeft: "-3.25rem",
+    },
+  },
+
   h1: {
-    fontSize: "xxl",
+    marginTop: "3rem",
+    fontSize: "$xxl",
     color: "$gray100",
   },
 
   p: {
+    marginTop: "1.5rem",
     fontSize: "xl",
     color: "$gray200",
     maxWidth: 560,
     textAlign: "center",
-    marginTop: "2rem",
     lineHeight: 1.4,
   },
+
   a: {
-    marginTop: "5rem",
+    marginTop: "4rem",
     display: "block",
     fontSize: "$lg",
     color: "$green500",
@@ -41,11 +51,12 @@ export const SuccessContainer = styled("main", {
 });
 
 export const ImageContainer = styled("div", {
-  width: "100%",
-  maxWidth: 130,
-  height: 145,
+  width: 140,
+  height: 140,
   background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
-  borderRadius: 8,
+  borderRadius: "50%",
+  boxShadow: "0px 0px 60px rgba(0, 0, 0, 0.8)",
+
   padding: "0.25rem",
   marginTop: "4rem",
 
